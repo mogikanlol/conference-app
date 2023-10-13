dfb11e7f1491bb8ed54604cb2b9cb0f8d4a6368a
Dmitrii Nikolaev <mogikanlol@gmail.com>
2023-10-14 06:20:12 +0900

    ConferenceRequestController Refactoring

--- backend/conference-crud-app/src/main/java/com/nikolaev/conference_request/ConferenceRequestResource.java
+++ backend/conference-crud-app/src/main/java/com/nikolaev/conference_request/ConferenceRequestResource.java
@@ -8,44 +8,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/requests")
 @RequiredArgsConstructor
-public class ConferenceRequestController {
+public class ConferenceRequestResource {
 
     private final ConferenceRequestService conferenceRequestService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity createRequest(@RequestBody ConferenceRequestDto requestDTO) {
+    @PostMapping
+    public ConferenceRequestDto createRequest(@RequestBody ConferenceRequestDto requestDTO) {
         log.info("createRequest() is invoked");
         log.info(requestDTO.toString());
         conferenceRequestService.createRequest(requestDTO);
-        return ResponseEntity.ok(requestDTO);
+        return requestDTO;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Page<BriefConferenceRequestDto> getAll(@RequestParam(value = "status", required = false) Integer statusNumber,
+    @GetMapping
+    public Page<BriefConferenceRequestDto> getAll(@RequestParam(name = "status", required = false) Integer statusNumber,
                                                   @PageableDefault Pageable pageable) {
         return conferenceRequestService.getAll(statusNumber, pageable);
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public void update(@RequestBody ConferenceRequestDto requestDTO) {
         conferenceRequestService.update(requestDTO);
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @GetMapping("{id}")
     public ConferenceRequestDto getRequest(@PathVariable("id") Long requestId) {
         return conferenceRequestService.getRequest(requestId);
     }
 
-    @RequestMapping(value = "{id}/comments", method = RequestMethod.POST)
+    @PostMapping("{id}/comments")
     public ConferenceRequestDto createComment(@RequestBody ConferenceRequestCommentDto commentDto,
-                                        @PathVariable("id") Long requestId) {
+                                              @PathVariable("id") Long requestId) {
         return conferenceRequestService.createComment(requestId, commentDto);
     }
 }
